0aa88720addf4fdff0fd5e6cde2821e65fb41d6c
范德萨 <7367191+ffffffds@user.noreply.gitee.com>
2023-01-07 21:41:08 +0800

    update

--- functions/authLogin.js
+++ functions/authLogin.js
@@ -7,7 +7,7 @@ export const onRequest = async ({ request, next, env }) => {
         params.append('client_id', '107571405')
         params.append('client_secret', '31ffa915289d6da3751ee4a4273d5713674e742b4e5108574deee0647770f6ed')
         params.append('redirect_uri', 'https://www.onecourse.top/CourseView')
-        return await env.ASSETS.fetch('https://oauth-login.cloud.huawei.com/oauth2/v3/token', {
+        return await fetch('https://oauth-login.cloud.huawei.com/oauth2/v3/token', {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded'
             },
