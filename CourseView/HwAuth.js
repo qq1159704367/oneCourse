54fb304cb204087b0a897959ad1e41f9bade835d
范德萨 <7367191+ffffffds@user.noreply.gitee.com>
2023-02-01 19:10:54 +0800

    update

--- CourseView/HwAuth.js
+++ CourseView/HwAuth.js
@@ -170,7 +170,7 @@ const Hw_uploadToCloud = function (isApp, items, callback) {
                     $.ajax({
                         url: url,
                         contentType: 'multipart/related;boundary=' + boundary,
-                        method: nameToId[item.name] ? 'put' : 'post',
+                        method: 'post',
                         headers: {
                             'Authorization': 'Bearer ' + token
                         },
