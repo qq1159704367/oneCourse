3fbf889ed017f5a7753d1594e5606f8d3d21fdc2
范德萨 <7367191+ffffffds@user.noreply.gitee.com>
2023-02-18 15:47:46 +0800

    update

--- functions/authLogin.js
+++ functions/authLogin.js
@@ -63,7 +63,8 @@ export const onRequest = async ({ request, next, env }) => {
                     headers: {
                         'Content-Type': 'application/json',
                         'Access-Control-Allow-Origin': 'https://ffffffds.gitee.io',
-                        'Access-Control-Allow-Methods': 'POST,OPTIONS'
+                        'Access-Control-Allow-Methods': 'POST,OPTIONS',
+                        'Access-Control-Allow-Headers': 'Content-Type,sec-ch-ua,sec-ch-ua-mobile,sec-ch-ua-platform'
                     }
                 })
             } else {
